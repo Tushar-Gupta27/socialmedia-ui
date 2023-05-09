e305957f7d8f6b502596d9ecf23f05f8abde0b71
Tushar Gupta <guptatusharhhfs27@gmail.com>
2023-05-10 00:42:50 +0530

    added UI files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import Navbar from "./components/Navbar/Navbar";
+import { Container } from "@mui/material";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import PostDetails from "./components/PostDetails/PostDetails";
+import { useSelector, useDispatch } from "react-redux";
 function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const tempUser = JSON.parse(localStorage.getItem("profile"));
+    if (tempUser) {
+      //two objects are never same so need better checks
+      if (tempUser !== user) dispatch({ type: "AUTH", payload: tempUser });
+    }
+  }, [location]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl" sx={{ padding: "0 0 20px 0" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/posts" />} />
+        <Route path="/posts" element={<Home />} />
+        <Route path="/posts/s" element={<Home />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="/posts" replace={true} /> : <Auth />}
+        />
+      </Routes>
+    </Container>
   );
 }
 
